a3e111b734dd2ba0bde67d4d4cfe4f794508d78f
Pawel W <elendil@konto.pl>
2022-12-13 21:10:50 +0100

    Circular buffer added - initial impl

---
+++ circularBuffer.c
@@ -0,0 +1,16 @@
+#include <inttypes.h>
+
+#define BUFFER_SIZE 128
+
+uint8_t buffer[BUFFER_SIZE];
+
+inline void storeCharacter( uint8_t c)
+{
+}
+
+uint16_t getMessage( uint8_t* message )
+{
+	uint16_t messageSize = 0;
+
+	return messageSize;
+}
---
+++ circularBuffer.h
@@ -0,0 +1,10 @@
+#ifndef CIRCULAR_BUFFER_H
+#define CIRCULAR_BUFFER_H
+
+#include <inttypes.h>
+
+inline void storeCharacter( uint8_t c);
+
+uint16_t getMessage( uint8_t* message );
+
+#endif
